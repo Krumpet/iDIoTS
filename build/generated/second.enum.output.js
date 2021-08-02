012402251e0366d1e5c67e7a9636490eadc6c4b0
Krumpet <ran.lottem@campus.technion.ac.il>
2021-08-02 20:20:35 +0300

    gitignore update

--- build/generated/second.enum.output.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.idiotsTokens = void 0;
-var idiotsTokens;
-(function (idiotsTokens) {
-    idiotsTokens["ToBeImplemented"] = "ToBeImplemented";
-    idiotsTokens["AnotherInterface"] = "AnotherInterface";
-})(idiotsTokens = exports.idiotsTokens || (exports.idiotsTokens = {}));
-//# sourceMappingURL=second.enum.output.js.map
